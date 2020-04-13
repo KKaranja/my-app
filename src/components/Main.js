3793d0a9226a11fda3872d01f4fb070099a17cff
isaac karanja <isaackimura@gmail.com>
2020-04-13 14:46:01 +0300

    adding a form to the main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './App.css';
 
 const Main = () =>{
     return(
         <div>
-            <h1>Hello Wordld!</h1>
+            
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <h1>Hello Wordld!</h1>
+                    <form action="" onSubmit={this.handleSubmit} noValidate>
+                        <div className="population">
+                            <label htmlFor="pupulation" className="">pupulation</label>
+                            <input type="text" className="" placeholder="pupulation" type="text" name="pupulation" noValidate onChange={this.handleChange}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
