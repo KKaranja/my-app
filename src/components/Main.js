483493b701d255509d495ebb62823f435d8b28f4
isaac karanja <isaackimura@gmail.com>
2020-04-13 14:58:40 +0300

    adding a form

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ const Main = () =>{
             <div className="wrapper">
                 <div className="form-wrapper">
                     <h1>Hello Wordld!</h1>
-                    <form action="" onSubmit={this.handleSubmit} noValidate>
+                    <form action="" >
                         <div className="population">
                             <label htmlFor="pupulation" className="">pupulation</label>
                             <input type="text" className="" placeholder="pupulation"  name="pupulation" />
