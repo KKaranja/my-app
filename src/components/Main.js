990fcf60ede626e05a3643d918d64168a2158bcf
isaac karanja <isaackimura@gmail.com>
2020-04-13 20:53:25 +0300

    adding font family

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css';
+import dropdown from './Dropdown';
 
 
 const Main = () =>{
@@ -11,19 +12,28 @@ const Main = () =>{
                     <h1>Fill In Your Custom Data</h1>
                     <form action="" >
                         <div className="population">
+                            <label >Pupulation </label>
                             <input type="text" className="" placeholder="Pupulation"  name="pupulation" />
                         </div>
                         <div className="time-to-elapse">
+                            <label >Time To Elapse</label>
                             <input type="text" className="" placeholder="Time To Elapse" name="time to elapse" />
                         </div>
                         <div className="requested-cases">
+                            <label >Requested Cases</label>
                             <input type="text" className="" placeholder="Requested Cases" name="requested cases" />
                         </div>
                         <div className="total-hospital-beds">
+                            <label >Total Hospital Beds</label>
                             <input type="text" className="" placeholder="Total Hospital Beds" name="Total beds" />
                         </div>
+                        <button className="dropdown-btn">Dropdown
+                                <i className="fa fa-caret-down"></i>
+                        </button>
                         <div className="period-type">
-                            <input type="text" className="" placeholder="Select Period Type" name="period type" />
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
                         </div>
                         
                     </form>
